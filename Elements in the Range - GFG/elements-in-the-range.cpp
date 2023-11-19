68469b7a24096838f6b23fc3fa538970a8025411
SoumyaAgrawal1 <113857023+SoumyaAgrawal1@users.noreply.github.com>
2023-11-19 22:11:13 +0530

    Added solution - LeetHub

---
+++ Elements in the Range - GFG/elements-in-the-range.cpp
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	
+	
+	public:
+	bool check_elements(int arr[], int n, int A, int B)
+	{
+	    unordered_map<int,int>mp;
+		for(int i=0;i<n;i++){
+		    mp[arr[i]]++;
+		}
+		for(int i=A;i<=B;i++)
+		{
+		  if(mp.find(i)==mp.end())  
+		     return 0;
+		     
+		}	
+		return 1;
+	}
+
+};
+	
+
+//{ Driver Code Starts.
+
+
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+    	int n,A,B;
+		cin>>n>>A>>B;
+		int a[n];
+		for(int i=0;i<n;++i)
+			cin>>a[i];
+
+        
+
+        Solution ob;
+        if (ob.check_elements(a, n, A, B))
+			cout << "Yes";
+		else
+			cout << "No";
+	    
+        
+	    cout << "\n";
+	     
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+// } Driver Code Ends
