66446de06b561147218d21ef0b6fc7adc1a973e5
SoumyaAgrawal1 <113857023+SoumyaAgrawal1@users.noreply.github.com>
2024-05-07 11:14:18 +0530

    Time: 0 ms, Memory: 7.5 MB - LeetHub

---
+++ sqrtx/sqrtx.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+long long int bs(int x){
+     int s=1;
+        int e=x;
+        long long int ans=0;
+        int mid=s+(e-s)/2;
+
+        while(s<=e){
+            if(mid==x/mid)
+             return mid;
+            else if(mid<x/mid)
+             {
+                 s=mid+1;
+                 ans=mid;
+             }
+             else if(mid>x/mid)
+              {
+                  e=mid-1;    
+              }
+            mid=s+(e-s)/2;
+        }
+        return ans;
+}
+    int mySqrt(int x) {
+        if(x==0)return 0;
+       return bs(x);
+    }
+};
